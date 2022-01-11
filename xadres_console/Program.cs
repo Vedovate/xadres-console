3f5b7be4ed16241edd4f6922df15b11bb4b94e66
Leonardo Vedovate <lvedovate@outlook.com>
2022-01-11 06:49:30 -0300

    projeto está pegando forma

--- xadres_console/Program.cs
+++ xadres_console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tabuleiro;
+using System;
+using xadrez_console;
+using xadrez;
+
+
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
+
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 9));
+
+    Tela.imprimirTabuleiro(tab);
+}
+catch (TabuleiroException ex) {
+    Console.WriteLine(ex.Message);
+}
+
